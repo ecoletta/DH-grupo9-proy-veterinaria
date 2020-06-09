0d05e4e9da37fe4d6e18b0121307be20a1e37b84
ecoletta <ezequielcoletta@hotmail.com>
2020-06-09 16:06:33 -0300

    Se agregan las configuraciones de CRUD de usuario y subida de fotos de perfil

--- site/myApp/routes/users.js
+++ site/myApp/routes/users.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+// Para la carga de archivos con multer
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/img_users')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+})
+ 
+var upload = multer({ storage: storage });
 
 // CONTROLADORES
 const usersController = require('../controllers/usersController')
@@ -8,6 +22,9 @@ const usersController = require('../controllers/usersController')
 router.get('/registro/', function(req, res, next) {
   res.render('registro');
 });
+
+/* POST Registro usuarios /users/registro. */
+router.post('/registro/', upload.any(), usersController.store);
 
 /* GET usuarios /users */
 router.get('/', usersController.root);
