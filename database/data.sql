b15d57b7251e1f436d87d95293e3033c28e2d605
ecoletta <ezequielcoletta@hotmail.com>
2020-06-30 15:52:48 -0300

    ORM:Models terminados y relaciones

--- database/data.sql
+++ database/data.sql
@@ -5,7 +5,7 @@ CREATE TABLE `veterinaria`.`usuarios` (
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
   `category` VARCHAR(15) NOT NULL,
   `image` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`));
